3a071f64196bcb3a6d800020f1f6beeb040086b8
Eliane Gerber <eliane.gerber@stud.fhgr.ch>
2025-04-16 15:16:40 +0200

    scrip.js

--- code-alongs/01_passwort_checker/script.js
+++ code-alongs/01_passwort_checker/script.js
@@ -1,1 +1,26 @@
 console.log('hi script.js')
+
+let password = prompt("Passwort eingeben")
+let username = prompt("Username eingeben")
+checkStringLengt(password, 12);
+checkStringLengt(username, 7)
+
+function checkPasswort (pw) {
+checkPasswort(password);
+checkPasswort(username);
+    if (password. length <11) {
+        console.log("Das Passwort ist lang genug!")
+         } else {
+        console.log("Bitte verwende ein längeres Passwort.")
+    }
+}
+
+function checkStringLengt(testString, testLength) {
+    if (testString.length >= testLength) {
+        console.log("Der Text ist lang genug");
+    } else {
+        console.log("Bitte verwende einen längeren Text.");
+    }
+
+    
+}
