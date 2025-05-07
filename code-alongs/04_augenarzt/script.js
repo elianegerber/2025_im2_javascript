72cf56834b3b280b2a70b2d4b9047dda9cc0a807
Eliane Gerber <eliane.gerber@stud.fhgr.ch>
2025-05-07 09:44:36 +0200

    yes

--- code-alongs/04_augenarzt/script.js
+++ code-alongs/04_augenarzt/script.js
@@ -1,1 +1,10 @@
 console.log('hi script.js')
+const auge = document.querySelector("eye")
+
+auge.addEventListener("click",function (event){ 
+    auge.classList.toggle("bloddy")
+});
+
+setInterval(function () {
+    auge.classLisst.toggle("closed");
+}, 2000);
--- code-alongs/08_motivator/script.js
+++ code-alongs/08_motivator/script.js
@@ -1,1 +1,36 @@
-console.log('hi script.js')
+const nameField = document.querySelector("#firstname");
+const skillField = document.querySelector("#skills");
+let skillInput = document.querySelector("#add");
+
+function getAndSetName () {
+    let nameLocalStorage = localStorage.getItem("name");
+    if (nameLocalStorage) {
+      nameField.innerText = nameLocalStorage;
+    } else {
+        let nameInput = prompt ("Bitte gib deinen Namen an:");
+        nameField.innerText = nameInput;
+        localStorage.setItem("name", nameInput);
+}
+}
+function getAndSetSkillInput() {
+    let skills = [];
+    skillInput.addEventListener("change", function() {
+        skills.push(skillInput.value);
+        console.log(skills);
+        skillField.innerText = skills;
+        skillInput.value = "";
+    });
+}
+function getSkills() {
+    skillInput.addEventListener("change", function( {
+        skills.push(skillInput.value);
+        skillInput.value = "";
+    }
+    ))
+}
+function set
+
+//do some stuff
+getAndSetName();
+getAndSetSkillInput();
+getSkills ();
